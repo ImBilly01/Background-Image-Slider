2ca375d04a46acc506d3c7ff5d6301ec9427d191
ImBilly <56002652+ImBilly01@users.noreply.github.com>
2021-07-24 09:37:50 +0200

    Add files via upload

---
+++ Background Image Slider Project/index.js
@@ -0,0 +1,39 @@
+const left = document.getElementById('left');
+const right = document.getElementById('right');
+
+let imageCont = document.getElementById('image-container');
+let counter = 0;
+
+const pictures = [
+    `url('./img/containerImage-0.jpeg')`,
+    `url('./img/containerImage-1.jpeg')`,
+    `url('./img/containerImage-2.jpeg')`,
+    `url('./img/containerImage-3.jpeg')`,
+    `url('./img/containerImage-4.jpeg')`
+];
+
+left.addEventListener('click', () => {
+    counter++;
+
+    if (counter > pictures.length -1) {
+        counter = 0;
+    }
+
+    document.body.style.backgroundImage = pictures[counter];
+    document.body.style.backgroundRepeat = 'no-repeat';
+    document.body.style.backgroundPosition = 'center';
+    document.body.style.backgroundSize = '100%';
+});
+
+right.addEventListener('click', () => {
+    counter--;
+
+    if (counter < 0) {
+        counter = pictures.length -1;
+    }
+
+    document.body.style.backgroundImage = pictures[counter];
+    document.body.style.backgroundRepeat = 'no-repeat';
+    document.body.style.backgroundPosition = 'center';
+    document.body.style.backgroundSize = '100%';
+});
